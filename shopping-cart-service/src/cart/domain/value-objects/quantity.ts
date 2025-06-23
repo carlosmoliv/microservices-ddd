166060696559ab3c7ec3894a1df427272f7ea269
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-06-22 22:27:44 -0300

    refactor(cart): remove ValueObject inheritance and simplify Quantity class

--- shopping-cart-service/src/cart/domain/value-objects/quantity.ts
+++ shopping-cart-service/src/cart/domain/value-objects/quantity.ts
@@ -1,11 +1,17 @@
-import { ValueObject } from './value-object';
 import { InvalidQuantityError } from '../errors/invalid-quantity.error';
 
-export class Quantity extends ValueObject<number> {
-  constructor(value: number) {
+export class Quantity {
+  constructor(readonly value: number) {
     if (value < 0) {
       throw new InvalidQuantityError(value);
     }
-    super(value);
+  }
+
+  equals(other: Quantity): boolean {
+    return this.value === other.value;
+  }
+
+  toJson(): { value: number } {
+    return { value: this.value };
   }
 }
