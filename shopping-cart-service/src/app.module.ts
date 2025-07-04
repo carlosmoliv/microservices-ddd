4b0ddeb9f53f2c78a353eb162273b604d9643730
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-07-04 17:50:34 -0300

    chore(cart): configure global settings with Joi validation for PORT and RABBITMQ_URI

--- shopping-cart-service/src/app.module.ts
+++ shopping-cart-service/src/app.module.ts
@@ -1,9 +1,21 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CartModule } from './cart/cart.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        RABBITMQ_URI: Joi.string().uri().required(),
+      }),
+    }),
+    CartModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
