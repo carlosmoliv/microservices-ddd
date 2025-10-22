ba331a2270c7b92abf916e14dd5fe5e584734f5a
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-10-21 23:24:24 -0300

    feat(cart): add CartItemQuantityUpdatedEvent

---
+++ shopping-cart-service/src/main/java/com/carlosoliveira/ecommerce/shoppingcart/application/events/CartItemQuantityUpdatedEvent.java
@@ -0,0 +1,18 @@
+package com.carlosoliveira.ecommerce.shoppingcart.application.events;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record CartItemQuantityUpdatedEvent(
+        UUID cartId,
+        UUID userId,
+        UUID productId,
+        int previousQuantity,
+        int newQuantity,
+        int quantityDifference,
+        Instant occurredAt
+) {
+    public CartItemQuantityUpdatedEvent {
+        quantityDifference = newQuantity - previousQuantity;
+    }
+}
